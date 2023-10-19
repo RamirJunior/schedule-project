ccfb9e5d181176b2313b49fe6ab9ee8be6d90537
Ramir Junior <junior.ribeiro0@live.com>
2023-10-19 10:44:58 -0300

    initial project commit

---
+++ src/test/java/ramir/com/schedule/ScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package ramir.com.schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
